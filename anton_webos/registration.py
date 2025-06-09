b56b606f7428a6ae1fed1bc8f784b779b3548da5
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2025-06-09 20:26:54 +0530

    Bug fix: Remove obsolete args.

--- anton_webos/registration.py
+++ anton_webos/registration.py
@@ -92,8 +92,7 @@ class WebOsRegistrationController:
             raise ResourceNotFound(device_id)
 
         Thread(target=self.process_registration,
-               args=(device_id, device_info, device_info['store'],
-                     registration_generator, callback)).start()
+               args=(device_id, device_id, callback)).start()
 
     def process_registration(self, device_id, callback):
         device_info = self.conns[device_id]
